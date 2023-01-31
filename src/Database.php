872787112468a02c27f21bf5e1b758f30cdca6a5
Crefito 11 <informatica@crefito11.org.br>
2023-01-31 16:50:00 -0300

    updated classes and namespace

---
+++ src/Database.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace RubensDimas\DatabaseManager;
+
+use \PDO;
+use \PDOException;
+
+class Database{
+
+  /**
+   * Instancia de conexão
+   * @var PDO
+   */
+  private $connection;
+
+  /**
+   * Método responsável por retornar a conexão
+   * @return PDO
+   */
+  public function getConnection(){
+    return $this->connection;
+  }
+
+  /**
+   * Método responsável por setar a conexão com o banco de dados
+   * @param PDO
+   * @return PDO
+   */
+  public function setConnection($connection){
+    $this->connection = $connection;
+  }
+
+  /**
+   * Método responsável por executar queries dentro do banco de dados
+   * @param  string $query
+   * @param  array  $params
+   * @return PDOStatement
+   */
+  public function execute($query,$params = []){
+    try{
+
+      /**
+       * Setando a conexão
+       */
+      $this->setConnection(DatabaseConnector::getDBConnection());
+
+      $statement = $this->getConnection()->prepare($query);
+      $statement->execute($params);
+      return $statement;
+    }catch(PDOException $e){
+      die('ERROR: '.$e->getMessage());
+    }
+  }
+
+  /**
+   * Método responsável por inserir dados no banco
+   * @param  array $values [ field => value ]
+   * @return integer ID inserido
+   */
+  public function insert($table,$values){
+    //DADOS DA QUERY
+    $fields = array_keys($values);
+    $binds  = array_pad([],count($fields),'?');
+
+    //MONTA A QUERY
+    $query = 'INSERT INTO '.$table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+
+    //EXECUTA O INSERT
+    $this->execute($query,array_values($values));
+
+    //RETORNA O ID INSERIDO
+    return $this->getConnection()->lastInsertId();
+  }
+
+  /**
+   * Método responsável por executar uma consulta no banco
+   * @param  string $where
+   * @param  string $order
+   * @param  string $limit
+   * @param  string $fields
+   * @return PDOStatement
+   */
+  public function select($table = null, $where = null, $order = null, $limit = null, $fields = '*'){
+    //DADOS DA QUERY
+    $where = strlen($where) ? 'WHERE '.$where : '';
+    $order = strlen($order) ? 'ORDER BY '.$order : '';
+    $limit = strlen($limit) ? 'LIMIT '.$limit : '';
+
+    //MONTA A QUERY
+    $query = 'SELECT '.$fields.' FROM '.$table.' '.$where.' '.$order.' '.$limit;
+
+    //EXECUTA A QUERY
+    return $this->execute($query);
+  }
+
+  /**
+   * Método responsável por executar atualizações no banco de dados
+   * @param  string $where
+   * @param  array $values [ field => value ]
+   * @return boolean
+   */
+  public function update($table,$where,$values){
+    //DADOS DA QUERY
+    $fields = array_keys($values);
+
+    //MONTA A QUERY
+    $query = 'UPDATE '.$table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
+
+    //EXECUTAR A QUERY
+    $this->execute($query,array_values($values));
+
+    //RETORNA SUCESSO
+    return true;
+  }
+
+  /**
+   * Método responsável por excluir dados do banco
+   * @param  string $where
+   * @return boolean
+   */
+  public function delete($table,$where){
+    //MONTA A QUERY
+    $query = 'DELETE FROM '.$table.' WHERE '.$where;
+
+    //EXECUTA A QUERY
+    $this->execute($query);
+
+    //RETORNA SUCESSO
+    return true;
+  }
+
+
+}
---
+++ src/DatabaseConnector.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace RubensDimas\DatabaseManager;
+
+use \PDO;
+use \PDOException;
+
+class DatabaseConnector
+{
+
+  /**
+   * Host de conexão com o banco de dados
+   * @var string
+   */
+  private static $host;
+
+  /**
+   * Nome do banco de dados
+   * @var string
+   */
+  private static $name;
+
+  /**
+   * Usuário do banco
+   * @var string
+   */
+  private static $user;
+
+  /**
+   * Senha de acesso ao banco de dados
+   * @var string
+   */
+  private static $pass;
+
+  /**
+   * Porta de acesso ao banco
+   * @var integer
+   */
+  private static $port;
+
+  /**
+   * Instancia de conexão com o banco de dados
+   * @var PDO
+   */
+  private static $connection;
+
+  /**
+   * Instancia da própria classe
+   */
+  private static $instance;
+
+  /**
+   * Método responsável por configurar a classe
+   * @param  string  $host
+   * @param  string  $name
+   * @param  string  $user
+   * @param  string  $pass
+   * @param  integer $port
+   */
+  public static function config($host, $name, $user, $pass, $port = 3306)
+  {
+    self::$host = $host;
+    self::$name = $name;
+    self::$user = $user;
+    self::$pass = $pass;
+    self::$port = $port;
+  }
+
+  /**
+   * Define a tabela e instancia e conexão
+   * @param string $table
+   */
+  private final function __construct()
+  {
+    self::$connection = new PDO('mysql:host=' . self::$host . ';dbname=' . self::$name . ';port=' . self::$port, 'root', '') or die('Banco de Dados não conectado');
+  }
+
+  /**
+   * Método responsável por criar uma conexão com o banco de dados
+   */
+  public static function getDBConnection()
+  {
+    if (!isset(self::$instance)) {
+      self::$instance = new DatabaseConnector;
+    }/*  else {
+      echo "Banco de dados já está conectado";
+    } */
+
+    return self::$connection;
+  }
+}
+
+// Primeira conexão
+
+/* $conn = DatabaseConnector::getDBConnection();
+
+// Segunda conexão
+$conn = DatabaseConnector::getDBConnection();
+
+exit;   */
+
+
+
